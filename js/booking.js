7ff1ca588d75b1ccf2fe3094bdd3f4018a660839
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-08 06:56:53 +0700

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,5 +1,19 @@
-document.getElementById("bookingForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  window.open("https://zalo.me/0778307889","_blank");
-});
+function bookZalo() {
+  const date = document.getElementById("date").value;
+  const time = document.getElementById("time").value;
+  const guest = document.getElementById("guest").value;
 
+  let msg = "BOOKING THE WANN%0A";
+
+  if (date) msg += "📅 Date: " + date + "%0A";
+  if (time) msg += "⏰ Time: " + time + "%0A";
+  if (guest) msg += "👥 Guests: " + guest + "%0A";
+
+  msg += "%0A🔥 VIP TABLE";
+
+  // Zalo OA / cá nhân
+  const phone = "0778307889";
+  const url = `https://zalo.me/${phone}?text=${msg}`;
+
+  window.open(url, "_blank");
+}
