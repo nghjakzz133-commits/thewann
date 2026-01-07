a93f0baba094d108bff8bcc7164cecae1265d1c8
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-01-08 06:52:18 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+let currentLang = localStorage.getItem("lang") || "vi";
+
+function setLang(lang) {
+  currentLang = lang;
+  localStorage.setItem("lang", lang);
+  applyLang();
+}
+
+function applyLang() {
+  document.querySelectorAll("[data-lang]").forEach(el => {
+    const key = el.getAttribute("data-lang");
+    if (LANG[currentLang] && LANG[currentLang][key]) {
+      el.innerText = LANG[currentLang][key];
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", applyLang);
 
