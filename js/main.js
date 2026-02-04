129a4bb24be890e30f43a65e6d3fc3f0e9f68917
nghjakzz133-commits <nghjakzz133@gmail.com>
2026-02-05 03:19:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -28,3 +28,11 @@ if (isTikTok) {
     });
   });
 }
+
+ const toggle = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.main-nav');
+
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
